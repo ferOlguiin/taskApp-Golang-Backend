94cd6ac8a3255d7440e64c7ac1a541e0ebb62f73
ferOlguiin <feergui997@hotmail.com>
2023-04-11 01:07:00 -0300

    more

--- routes/loginuser.go
+++ routes/loginuser.go
@@ -73,8 +73,8 @@ func LoginUser(c *gin.Context) {
 	}
 
 	c.SetSameSite(http.SameSiteNoneMode)
-	c.SetCookie("Auth", token, 24000, "/", "", true, true)
-	c.SetCookie("CheckAuth", "SiAutentico", 24000, "/", "", true, true)
+	c.SetCookie("Auth", token, 24000, "/", "task-app-amvt.onrender.com/", true, true)
+	c.SetCookie("CheckAuth", "SiAutentico", 24000, "/", "task-app-amvt.onrender.com/", true, true)
 
 	c.JSON(http.StatusCreated, user)
 }
