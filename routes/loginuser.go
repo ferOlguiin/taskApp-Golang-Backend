a4a4f7e43aa521f29cfedbbffcdb7a2a080416fc
ferOlguiin <feergui997@hotmail.com>
2023-04-11 18:09:18 -0300

    asiodhaoiw

--- routes/loginuser.go
+++ routes/loginuser.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"context"
+	"fmt"
 	"golangGinMongo/collection"
 	"golangGinMongo/database"
 	"golangGinMongo/model"
@@ -74,6 +75,10 @@ func LoginUser(c *gin.Context) {
 
 	c.SetSameSite(http.SameSiteNoneMode)
 	c.SetCookie("Auth", token, 24000, "/", "https://task-app-amvt.onrender.com", true, true)
+
+	cookkk, errorrr := c.Request.Cookie("Auth")
+	fmt.Println("la cookie es: ", cookkk)
+	fmt.Println("el error es: ", errorrr)
 	//c.SetCookie("CheckAuth", "SiAutentico", 24000, "/", "https://task-app-amvt.onrender.com", true, false)
 	c.JSON(http.StatusCreated, user)
 }
