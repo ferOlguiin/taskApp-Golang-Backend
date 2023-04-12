7375f84ef554d6e4fae853f5088f15f369d1015d
ferOlguiin <feergui997@hotmail.com>
2023-04-11 21:09:43 -0300

    ultima

--- routes/loginuser.go
+++ routes/loginuser.go
@@ -29,13 +29,13 @@ func LoginUser(c *gin.Context) {
 	if error == nil && cookieClient.Value != "" {
 		fmt.Println(cookieClient.Name, cookieClient.Value)
 		defer cancel()
-		// var cookie CookieSearched
-		// c.BindJSON(&cookie)
-		// fmt.Println("esta es la cookie q en teoria viene de front", cookie)
-		// if cookie.Value != cookieClient.Value {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"message": "No coinciden los datos de las cookies"})
-		// 	return
-		// }
+		var cookie CookieSearched
+		c.BindJSON(&cookie)
+		fmt.Println("esta es la cookie q en teoria viene de front", cookie)
+		if cookie.Value != cookieClient.Value {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "No coinciden los datos de las cookies"})
+			return
+		}
 		token, notoken := c.Request.Cookie("Auth")
 		fmt.Println("esto es el valor de cookie de auth", token)
 		if notoken != nil {
